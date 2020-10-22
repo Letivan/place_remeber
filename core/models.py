7014924de413383291d6788d1d6ea8cfd14b9c10
kii96 <1407navi@gmail.com>
2020-10-22 14:47:34 +0500

    Create model Remember
    The model for the storage of memories in the places you visit.
    Required: binding to the user, memory text, and coordinates.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
 from django.db import models
+from geoposition.fields import GeopositionField
 
-# Create your models here.
+
+class Remember(models.Model):
+    """The model for the storage of memories in the places you visit"""
+    date_create = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, verbose_name='Пользователь', db_index=True)
+    remember = models.TextField(verbose_name='Воспоминание')
+    coordinate = GeopositionField(verbose_name='Координаты')
+
+    def __str__(self):
+        return self.user.first_name
+
+    class Meta:
+        verbose_name = 'Воспоминание'
+        verbose_name_plural = "Воспоминания"
+        ordering = ('-date_create',)

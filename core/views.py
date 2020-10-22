b4363f561082760409ff18b03069fff2154e0e8a
kii96 <1407navi@gmail.com>
2020-10-22 16:17:03 +0500

    Add view LoginView

--- core/views.py
+++ core/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic import CreateView
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+from core.forms import RememberForm
+from core.models import Remember
+
+
+class LoginView(TemplateView):
+    """View The Registration/Authorization"""
+    template_name = 'login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        """If the user is logged in redirect to the home page"""
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super(self.__class__, self).dispatch(request, *args, **kwargs)
+

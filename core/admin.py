0f3fd64bfd948627d5458664bee522bfbb27427e
kii96 <1407navi@gmail.com>
2020-10-22 14:50:24 +0500

    Add model Remember in Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Remember
+
+
+@admin.register(Remember)
+class RememberAdmin(admin.ModelAdmin):
+    """Отображение модели Remember в админке, добавлено отображение полей для чтения в детальном просмотре"""
+    readonly_fields = ('id', 'date_create',)
